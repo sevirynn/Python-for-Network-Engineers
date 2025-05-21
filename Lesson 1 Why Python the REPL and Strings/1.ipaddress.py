d0f7a96f38128a980e16c275842017b464609344
Rynn Beaumont <sevirynncareer@tuta.com>
2025-05-21 09:27:35 -0400

    small refreshers

---
+++ Lesson 1 Why Python the REPL and Strings/1.ipaddress.py
@@ -0,0 +1,8 @@
+#!//usr/bin/env python3
+
+my_ip_adddr = input("Enter an IP address: ")
+
+print(my_ip_adddr)
+
+# The lessson asks for the bad habit of over explaining code since it's a beginner course
+# This comment is just for that requirement because the above is written very self explainatorily
---
+++ Lesson 1 Why Python the REPL and Strings/2.stringmethods.py
@@ -0,0 +1,5 @@
+#!//usr/bin/env python3
+
+my_var = input("Make a string! : ")
+
+print(my_var.upper())
